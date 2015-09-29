cc88be57633d2b0dc3e9c7d2670d2b400ac3ff40
Lawson Lamb <lawlzlamb@gmail.com>
2015-09-29 09:51:06 -0400

    Update 5

--- Assets/_Scripts/Cell.cs
+++ Assets/_Scripts/Cell.cs
@@ -5,136 +5,150 @@ using UnityEngine.UI;
 using Random = UnityEngine.Random;
 using UnityEngine.EventSystems;
 public enum Sprite_Type{
-	Red,
-	Blue,
-	Green,
-	Yellow,
-	Purple,
+	Red =0,
+	Blue=1,
+	Green =2 ,
+	Yellow = 3,
+	Purple = 4,
 
 }
-public class Cell : MonoBehaviour,IDropHandler {
-
+public class Cell : MonoBehaviour,IDropHandler,IPointerEnterHandler,IPointerExitHandler{
+	public int type;
 	//public SpriteRenderer _sr;
-	public int Column { get; set; }
-	public int Row { get; set; }
+	public int Column;
+	public int Row;
 	//public int TileType { get; set; }
-	//public string Type { get; set; }
-	//public bool IsMoving;
-	public Sprite sprite;
-	public Image image;
+	public Image containerImage;
+	public Image receivingImage;
 	public GridManager gridmanger;
+	private Color normalColor;
+	public Color highlightColor = Color.yellow;
 
-	void Start () {
-
-		//	_sr= gameObject.transform.FindChild ("Sprite").GetComponent<SpriteRenderer> () as SpriteRenderer;
-		image = gameObject.transform.FindChild ("Sprite").GetComponent<Image> ();
+	public void OnEnable ()
+	{
+		if (containerImage != null)
+			normalColor = containerImage.color;
+		
+	}
+	void Start(){
 		gridmanger = gameObject.transform.parent.GetComponent<GridManager> ();
 		int randomTile = Random.Range (0, gridmanger.sprites.Length);
-		image.sprite = gridmanger.sprites [randomTile];
-
-	//	_sr.sprite = sprite;
-
-	}
-	public Transform item {
-		get{
-			if (transform.childCount > 0) {
-				return transform.GetChild (0);
-			}
-			return null;
-				}
-
-	}
-
-	void Update () {
-		image = gameObject.transform.FindChild ("Sprite").GetComponent<Image> ();
-	
-	}
-
-	public void OnDrop(PointerEventData eventData){
-		/*
-		if (!item) {
-			Tile.itemBeingDragged.transform.SetParent (transform);
-		}
-		*/
-
-		Sprite DropSrite = GetDropSprite (eventData);
-		//Swap_Sprites (eventData, DropSrite);
-			
+		receivingImage.sprite = gridmanger.sprites [randomTile];
+		type  = randomTile;
 		
 	}
-	/*
-	public bool IsSameType(Cell otherShape)
-	{
-		if (otherShape == null || !(otherShape is Cell))
-			throw new ArgumentException("otherShape");
-
-		return string.Compare(this.Type, (otherShape as Cell).Type) == 0;
+	
+	void Update(){
+		
 	}
-
-*/
-	/// <param name="column"></param>
-	public void Assign(string type, int row, int column)
+	public void OnDrop(PointerEventData data)
 	{
-
-		if (string.IsNullOrEmpty(type))
-			throw new ArgumentException("type");
-
-		Column = column;
-		Row = row;
-		//Type = type;
+		containerImage.color = normalColor;
+		
+		if (receivingImage == null)
+			return;
+		
+		Sprite dropSprite = GetDropSprite (data);
+		if (dropSprite != null){
+			Swap_Sprites(data,dropSprite);
+			Check_Vertical();
+		}
+		//receivingImage.overrideSprite = dropSprite;
 	}
-
-	public static void SwapColumnRow(Cell a, Cell b)
+	
+	public void OnPointerEnter(PointerEventData data)
 	{
-		int temp = a.Row;
-		a.Row = b.Row;
-		b.Row = temp;
-
-		temp = a.Column;
-		a.Column = b.Column;
-		b.Column = temp;
+		if (containerImage == null)
+			return;
+		
+		
+		Sprite dropSprite = GetDropSprite (data);
+		if (dropSprite != null)
+			containerImage.color = highlightColor;
 	}
-
-	private Sprite GetDropSprite(PointerEventData eventData){
-		var originalObj = eventData.pointerDrag;
+	
+	public void OnPointerExit(PointerEventData data)
+	{
+		if (containerImage == null)
+			return;
+		
+		containerImage.color = normalColor;
+	}
+	
+	private Sprite GetDropSprite(PointerEventData data)
+	{
+		var originalObj = data.pointerDrag;
 		if (originalObj == null)
 			return null;
-
-		var cell = originalObj.GetComponent<Cell>();
-		if (cell == null)
+		
+		var dragMe = originalObj.GetComponent<Tile>();
+		if (dragMe == null)
 			return null;
 		
-		var srcImage = originalObj.GetComponent<Cell> ().image;
+		var srcImage = originalObj.GetComponent<Image>();
 		if (srcImage == null)
 			return null;
-		Sprite temp = image.sprite;
-		print (temp.name);
-		image.sprite = sprite;
-		print (image.sprite.name);
-		srcImage.sprite = temp;
-		print (temp.name);
-		print (srcImage.sprite.name);
+		
 		return srcImage.sprite;
 	}
-
 	private void Swap_Sprites(PointerEventData eventData,Sprite sprite){
 		var originalObj = eventData.pointerDrag;
-		var srcImage = originalObj.GetComponent<Cell> ().image;
+		var srcImage = originalObj.GetComponent<Image>();
 		//sets image to temp
 		//then assigns the sprite from dropped object image
 		//then we set the image of the droped sprite to the sprite that in the cell
-		Sprite temp = image.sprite;
-		image.sprite = sprite;
+
+
+		Sprite temp = receivingImage.sprite;
+		receivingImage.sprite = sprite;
 		srcImage.sprite = temp;
+		
+	}
+	[ContextMenu("Check")]
+	 void Check_Vertical(){
 
-
-
+		int temp1 = Row+1;
+		int temp2 = Row-1;
+		print ("check vert on : "+" ( "+  gameObject.name+") " + "row+1: "+ temp1+"  row-1: "+ temp2);
 	
 
+		if(gridmanger.Grid[Row+1,Column].GetComponent<Cell>().receivingImage.Equals(this.receivingImage)){
 
+			print (gameObject.name+ " : " + "vert_match" + Row+1);
+		}
 
+		if(gridmanger.Grid[Row+1,Column].GetComponent<Cell>().type ==type){
+
+			print (gameObject.name+ " : " + "vert_match" + Row+1);
+
+		}
+
+		
+		if(gridmanger.Grid[Row-1,Column].GetComponent<Cell>().receivingImage ==receivingImage){
+			int temp =  Row-1;
+			print (gameObject.name+ " : " + "vert_match" + temp.ToString());
+			
+		}
+
+	}
+	public void Assign(int row,int col){
+		Row = row;
+		Column = col;
 
 	}
 
+	[ContextMenu("PrintGrid")]
+	void printGridTEST(){
+
+		
+		for (int x = 0; x < gridmanger.GridWidth ; x++) {
+			for (int y = 0; y < gridmanger.GridHeight; y++) {
+				string temp = gridmanger.Grid[x,y].gameObject.name;
+				print (temp);
+			}
+		}
+
+
 	}
+}
 
--- Assets/_Scripts/GridManager.cs
+++ Assets/_Scripts/GridManager.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
-
+using Random = UnityEngine.Random;
 public class GridManager : MonoBehaviour {
 
 	public int GridWidth;
@@ -12,6 +12,7 @@ public class GridManager : MonoBehaviour {
 	public GameObject prefab;
 	//public Canvas canvas;
 	public GameObject panel;
+
 
 
 
@@ -26,6 +27,8 @@ public class GridManager : MonoBehaviour {
 				Grid[x,y] = Instantiate (prefab) as GameObject;
 				Grid[x,y].transform.SetParent (panel.transform, false);
 				Grid[x,y].name = "" + x + "," + y;
+				Grid[x,y].GetComponent<Cell>().Assign(x,y);
+
 				//RectTransform rectTrans = i.GetComponent<RectTransform> ();
 
 
@@ -34,9 +37,73 @@ public class GridManager : MonoBehaviour {
 			}
 
 			}
+
+	}
+	[ContextMenu("New Grid")]
+	void Respawn_Tiles(){
+
+		
+		for (int x = 0; x < GridWidth; x++) {
+			for (int y = 0; y < GridHeight; y++) {
+				Transform tile = Grid[x,y].transform.FindChild("Sprite");
+				int randomTile = Random.Range (0,sprites.Length);
+				tile.GetComponent<Image>().sprite = sprites [randomTile];
+
+			}
+
 	}
 
+	
 
+}
+
+	void CheckMatches(){
+
+		
+		for (int x = 0; x < GridWidth; x++) {
+			for (int y = 0; y < GridHeight; y++) {
+
+
+
+			}
+		}
+	
+
+
+	}
+
+	private void Check_Cols(GameObject go){
+
+	List<GameObject> matches = new List<GameObject>();
+
+		var cell = go.GetComponent<Cell>();
+
+		if (cell.Column!= 0){
+			for (int column = cell.Column - 1; column >= 0; column--)
+			{
+				if (Grid[cell.Row, column].GetComponent<Cell>().type == cell.type)
+				{
+					matches.Add(Grid[cell.Row, column]);
+				}
+				else
+					break;
+			}
+
+		}
+
+		if (cell.Column != GridWidth - 1)
+			for (int column = cell.Column + 1; column < GridWidth; column++)
+		{
+			if (Grid[cell.Row, column].GetComponent<Cell>().type ==  cell.type)
+			{
+				matches.Add(Grid[cell.Row, column]);
+			}
+			else
+				break;
+		}
+
+
+	}
 
 
 
